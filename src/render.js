4ae874514daa33c101cd2cb5995007dbf9b55c32
Marco Roganovic <marcoroganovic@gmail.com>
2017-09-29 21:39:40 +0200

    fix error in render

--- src/render.js
+++ src/render.js
@@ -2,6 +2,6 @@ import { isString } from "./is";
 
 export default (node, target) => {
   if(isString(target)) 
-    target.document.querySelector(target);
+    target = document.querySelector(target);
   target.appendChild(node);
 }
